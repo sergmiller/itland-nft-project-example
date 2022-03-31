411db9c17f47f34056fcf12cea70db0de05c7173
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-03-31 20:06:55 +0300

    * only owner can mint * rm hardcore in nft naming

--- contracts/ItlandNFT.sol
+++ contracts/ItlandNFT.sol
@@ -11,26 +11,18 @@ contract ItlandNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     uint256 public constant maxSupply = 10;
-    uint256 public constant mintPrice = 0.01 ether;
 
     Counters.Counter private _tokenCount;
 
-    constructor()
-        ERC721("Itland Cats NFT", "ITLANDCATS")
+    constructor(string memory _title, string memory _moto)
+        ERC721(_title, _moto)
     {}
-
-    modifier costs(uint price) {
-        if (msg.sender != owner()) {
-            require(msg.value >= mintPrice, "msg.value should be more or equal than price");
-        }
-        _;
-    }
 
     function widthdraw(address _address, uint256 amount) public payable onlyOwner {
         payable(_address).transfer(amount);
     }
 
-    function mint(string memory _tokenURI) public payable costs(mintPrice) {
+    function mint(string memory _tokenURI) public payable onlyOwner {
         uint256 newTokenId = _tokenCount.current();
         require(newTokenId < maxSupply);
          _tokenCount.increment();
