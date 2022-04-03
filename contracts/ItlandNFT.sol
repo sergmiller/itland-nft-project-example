81d52d81dc9cde7e26e44757964f1bd1455ff411
sergmiller <sergey.miller@phystech.edu>
2022-04-03 16:04:59 +0300

    Revert "Merge pull request #1 from AlcibiadesCleinias/preparation_for_mainnet"
    This reverts commit 6c456191abe9897519541022b79a6ccfbef68088, reversing
    changes made to b1a61c667956e45d6a0ef29400f48734e2eac0ec.

--- contracts/ItlandNFT.sol
+++ contracts/ItlandNFT.sol
@@ -10,18 +10,29 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract ItlandNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
+    uint256 public constant maxSupply = 10;
+    uint256 public constant mintPrice = 0.01 ether;
+
     Counters.Counter private _tokenCount;
 
-    constructor(string memory name_, string memory symbol_)
-        ERC721(name_, symbol_)
+    constructor()
+        ERC721("Itland Cats NFT", "ITLANDCATS")
     {}
+
+    modifier costs(uint price) {
+        if (msg.sender != owner()) {
+            require(msg.value >= mintPrice, "msg.value should be more or equal than price");
+        }
+        _;
+    }
 
     function widthdraw(address _address, uint256 amount) public payable onlyOwner {
         payable(_address).transfer(amount);
     }
 
-    function mint(string memory _tokenURI) public payable onlyOwner {
+    function mint(string memory _tokenURI) public payable costs(mintPrice) {
         uint256 newTokenId = _tokenCount.current();
+        require(newTokenId < maxSupply);
          _tokenCount.increment();
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
